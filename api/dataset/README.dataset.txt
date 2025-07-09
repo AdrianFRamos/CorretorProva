37e48cee6ef2dbe1c672e3ff577941c3994bbb51
AdrianFRamos <ferreiraadrian18@gmail.com>
2025-07-08 21:19:36 -0300

    quase

--- api/dataset/README.dataset.txt
+++
@@ -1,6 +0,0 @@
-# corretor-provas > Roboflow Instant 1 [Eval]
-https://universe.roboflow.com/adrianbranches/corretor-provas
-
-Provided by a Roboflow user
-License: CC BY 4.0
-
--- frontend/corretor_prova/lib/services/apiService.dart
+++ frontend/corretor_prova/lib/services/apiService.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 import 'package:path/path.dart';
 
 class ApiService {
-  static const String baseUrl = 'http://10.0.2.2:8000'; // Android Emulator
+  static const String baseUrl = 'http://172.25.3.135:8000'; // Android Emulator
 
   static Future<Map<String, dynamic>> enviarProva({
     required String turma,
